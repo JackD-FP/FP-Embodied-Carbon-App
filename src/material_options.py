a61f95b3926765aff42d4c776f2297a06d601b79
wakojako49 <95592023+JackD-FP@users.noreply.github.com>
2022-07-11 13:30:42 +1000

    update

--- src/material_options.py
+++ src/material_options.py
@@ -2,8 +2,6 @@ concrete = [
     {
         "label": "Concrete 50 MPa - GGBS",
         "value": "50_ggbs",
-        # "gb_label": "Concrete 50 MPa - GGBS  50%",
-        # "gb": 260.00,
         "epic_label": "Concrete 50 MPa - GGBS",
         "epic": 492.00,
         "ice_label": "Concrete 50 MPa - GGBS 50%",
@@ -13,8 +11,6 @@ concrete = [
     {
         "label": "Concrete 50 MPa - Fly Ash",
         "value": "50_fa",
-        # "gb_label": "Concrete 50 MPa - Fly Ash 50%",
-        # "gb": 363.00,
         "epic_label": "Concrete 50 MPa - Fly Ash",
         "epic": 467.00,
         "ice_label": "Concrete 50 MPa - Fly Ash 50%",
@@ -24,8 +20,6 @@ concrete = [
     {
         "label": "Concrete 50 MPa",
         "value": "50_mpa",
-        # "gb_label": "Concrete 50 MPa",
-        # "gb": 643.00,
         "epic_label": "Concrete 50 MPa",
         "epic": 600.00,
         "ice_label": "Concrete 50 MPa",
@@ -35,8 +29,6 @@ concrete = [
     {
         "label": "Concrete 40 MPa - GGBS",
         "value": "40_ggbs",
-        # "gb_label": "Concrete 40 MPa - GGBS  50%",
-        # "gb": 328.00,
         "epic_label": "Concrete 40 MPa - GGBS",
         "epic": 392.00,
         "ice_label": "Concrete 40 Mpa - GGBS 50%",
@@ -46,8 +38,6 @@ concrete = [
     {
         "label": "Concrete 40 MPa - Fly Ash",
         "value": "40_fa",
-        # "gb_label": "Concrete 40 MPa - Fly Ash  50%",
-        # "gb": 310.00,
         "epic_label": "Concrete 40 MPa - Fly Ash",
         "epic": 373.00,
         "ice_label": "Concrete 40 Mpa - Fly Ash 50%",
@@ -57,8 +47,6 @@ concrete = [
     {
         "label": "Concrete 40 MPa",
         "value": "40_mpa",
-        # "gb_label": "Concrete 40 MPa",
-        # "gb": 555.00,
         "epic_label": "Concrete 40 MPa",
         "epic": 497.00,
         "ice_label": "Concrete 40 MPa",
@@ -68,8 +56,6 @@ concrete = [
     {
         "label": "Concrete 32 MPa - GGBS",
         "value": "32_ggbs",
-        # "gb_label": "Concrete 32 MPa - GGBS 50%",
-        # "gb": 288.00,
         "epic_label": "Concrete 32 MPa - GGBS%",
         "epic": 331.00,
         "ice_label": "Concrete 32 MPa - GGBS 50%",
@@ -79,8 +65,6 @@ concrete = [
     {
         "label": "Concrete 32 MPa - Fly Ash",
         "value": "32_fa",
-        # "gb_label": "Concrete 32 MPa - Fly Ash 50%",
-        # "gb": 273.00,
         "epic_label": "Concrete 32 MPa - Fly Ash",
         "epic": 314.00,
         "ice_label": "Concrete 32 MPa - Fly Ash 50%",
@@ -90,8 +74,6 @@ concrete = [
     {
         "label": "Concrete 32 MPa",
         "value": "32_mpa",
-        # "gb_label": "Concrete 32 MPa",
-        # "gb": 481.00,
         "epic_label": "Concrete 32 MPa",
         "epic": 416.00,
         "ice_label": "Concrete 32 MPa",
@@ -105,8 +87,6 @@ rebar = [
     {
         "label": "Steel Reinforment Bar",
         "value": "rebar",
-        # "gb_label": "Standard - Bar Reinforcement",
-        # "gb": 2.9,
         "epic_label": "Steel reinforcement bar ",
         "epic": 2.9,
         "ice_label": "Steel Rebar recycled ",
@@ -119,8 +99,6 @@ steel = [
     {
         "label": "Steel Universal Section",
         "value": "steel_section",
-        # "gb_label": "Steel Universal Section",
-        # "gb": 2.9,
         "epic_label": "Steel Structural Steel Section",
         "epic": 3.88,
         "ice_label": "Steel Structural Steel Section",
@@ -130,8 +108,6 @@ steel = [
     {
         "label": "Steel Hollow Section",
         "value": "steel_hollow",
-        # "gb_label": "Steel Hollow Section",
-        # "gb": 4.6,
         "epic_label": "Steel Structural Steel Section",
         "epic": 4.6,
         "ice_label": "Steel Section Structural",
@@ -144,8 +120,6 @@ timber = [
     {
         "label": "Timber CLT",
         "value": "clt",
-        # "gb_label": "Cross Laminated Timber (CLT)",
-        # "gb": 645,
         "epic_label": "Cross Laminated Timber (CLT)",
         "epic": 645,
         "ice_label": "Timber CLT",
@@ -155,8 +129,6 @@ timber = [
     {
         "label": "Timber Glu-Lam",
         "value": "glulam",
-        # "gb_label": "Glue Laminated Timber (Glu-Lam)",
-        # "gb": 718,
         "epic_label": "Glue Laminated Timber (Glu-Lam)",
         "epic": 1718,
         "ice_label": "Timber Glulam",
@@ -166,8 +138,6 @@ timber = [
     {
         "label": "Timber LVL",
         "value": "lvl",
-        # "gb_label": "Laminated Veneer Lumber (LVL)",
-        # "gb": 590,
         "epic_label": "Laminated Veneer Lumber (LVL)",
         "epic": 1059,
         "ice_label": "Timber Glulam",
