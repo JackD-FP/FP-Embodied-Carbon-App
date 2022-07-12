79d89dd23c6353fc590cd09774ec7a3364d2cdd4
wakojako49 <95592023+JackD-FP@users.noreply.github.com>
2022-07-13 09:22:42 +1000

    update

---
+++ src/markdown.py
@@ -0,0 +1,15 @@
+from dash import html, dcc
+
+markdown_layout = dcc.Markdown(
+    """
+        ## Purpose of the tool
+
+        This tool aims to achieve a number of things;
+1.	Grow the general knowledge of Embodied Carbon within the architectural community.
+2.	Compare early stage design options for the embodied carbon of different structural solutions.
+3.	Cause individual users to ask further questions about where the carbon is and what to do about reducing it.
+4.	Help to build an industry wide conversation on how to build better now!
+
+
+    """
+)
