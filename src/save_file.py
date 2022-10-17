2ae5b34c568ddc0427452a1e93fe31c531391da0
wakojako49 <jackdumanat49@gmail.com>
2022-10-17 17:10:48 +1100

    working save file

--- src/save_file.py
+++ src/save_file.py
@@ -7,7 +7,7 @@ import pandas as pd
 from dash import Input, Output, State, callback, ctx, dcc, html
 from dash.exceptions import PreventUpdate
 from dash_iconify import DashIconify
-from firebase_admin import credentials, firestore
+from firebase_admin import auth, credentials, firestore, storage
 
 new_project = dmc.Tab(
     label="New Project",
@@ -50,14 +50,11 @@ existing_project = dmc.Tab(
                 dmc.Select(
                     label="Select framework",
                     placeholder="Select one",
-                    id="framework-select",
-                    value="ng",
-                    data=[
-                        {"value": "react", "label": "React"},
-                        {"value": "ng", "label": "Angular"},
-                        {"value": "svelte", "label": "Svelte"},
-                        {"value": "vue", "label": "Vue"},
-                    ],
+                    id="project_select",
+                    searchable=True,
+                    transition="slide-down",
+                    transitionDuration=0.5,
+                    maxDropdownHeight=200,
                 ),
             ],
             direction="column",
@@ -96,7 +93,8 @@ save_modal = html.Div(
                     id="save_to_firebase_btn",
                     disabled=True,
                 ),
-                html.Div(id="save_to_firebase_output"),
+                html.Div(id="save_new_project"),
+                html.Div(id="save_new_variation"),
             ],
             id="save-modal",
         ),
@@ -105,8 +103,28 @@ save_modal = html.Div(
 
 
 cred = credentials.Certificate("creds/creds.json")
-fp_app = firebase_admin.initialize_app(cred)
+fp_app = firebase_admin.initialize_app(
+    cred,
+    {
+        "storageBucket": "embodied-carbon.appspot.com",
+    },
+)
 db = firestore.client()
+bucket = storage.bucket(app=fp_app)
+
+
+# ----- disables save btn if no analysis -----
+@callback(
+    Output("save-button", "disabled"),
+    Input("proc_store", "data"),
+    prevent_initial_call=True,
+)
+def disable_save_btn(data):
+    if data is None:
+        return True
+    else:
+        return False
+
 
 # ----- get projects -----
 @callback(
@@ -124,47 +142,94 @@ def load_firestore_data(n_clicks):
     return projects
 
 
-# ----- Save Logic -----
-# TODO: CONSOLIDATE DATA TO SHORTEN IT
-# data is tool long. there my be a way to shorten it.
-def send_data(data, collection, document):
-    """Send data to firestore.
+# ----- ui handling for project_select -----
+def data_to_options(data):
+    """Converts data to options for Select component.
 
     Args:
-        data (_df_): pandas dataframe of data
-        collection (_Str_): Collection name
-        document (_Str_): Document name
+        data (_dict_): Dictionary of firestore data.
+
+    Returns:
+        _list_: returns list of dictionaries
     """
-    df = pd.read_json(data, orient="split")
-    to_send = df.to_dict("records")
-    to_send = df.to_json(orient="columns")
-    # for key in to_send.keys():
-    #     db.collection(collection).document(key).set({to_send[key]})
-    # db.collection(collection).document(document).set(data)
-    # print(to_send)
-    for i in to_send:
-        db.collection("{} + {}".format(collection, document)).add(i)
-
-
-"""
-# TODO: finish this function
-- add data to projects collection.
-- check if project name is unique.
-- if not append variation name to project name.
-def send_project_data(project_name, variation_name):
-
-    db.collection("projects").add(
-        {
-            "project_name": project_name,
-            "variation_name": [variation_name],
-            "date": [datetime.datetime.now(tz=datetime.timezone.utc)],
-        }
-    )
-"""
+    options = []
+    for key, value in data.items():
+        options.append({"value": key, "label": key})
+    return options
 
 
 @callback(
-    Output("save_to_firebase_output", "children"),
+    Output("project_select", "data"),
+    Input("save-button", "n_clicks"),
+    State("firebase_storage", "data"),
+    prevent_initial_call=True,
+)
+def update_project_select(n_clicks, data):
+    if data is None:
+        raise PreventUpdate
+    else:
+        options = data_to_options(data)
+        return options
+
+
+# ----- Save Logic -----
+# pd.groupby is used to reduce the size of the data that is saved to firestore
+def send_data(data, collection, document):
+    """Send data to firebase storage.
+
+    Args:
+        data ( dataframe ): pandas dataframe of data
+        collection ( String ): Collection name
+        document ( String ): Document name
+    """
+    df = pd.read_json(data, orient="split")
+    df_ = df.groupby(
+        [
+            "Floor Level",
+            "Element",
+            "Green Book Material",
+            "EPiC Material",
+            "ICE Material",
+        ],
+        as_index=False,
+    ).sum()
+    to_send = df_.to_json(orient="split")
+    # storing main json database in firebase storage
+    bucket.blob("{}+{}.json".format(collection, document)).upload_from_string(
+        data=to_send,
+        content_type="application/json",
+    )
+
+
+def new_project_(project_name, variation_name):
+    """Create new project in firestore.
+
+    Args:
+        project_name ( String ): Name of the project
+        variation_name ( String ): Name of the variation
+    """
+    db.collection("projects").document(project_name).set(
+        {
+            "project_name": project_name,
+            "variation_name": [variation_name],
+        }
+    )
+
+
+def append_to_project(project_name, variation_name):
+    """Append variation name to project.
+
+    Args:
+        project_name ( String ): Name of project
+        variation_name ( String ): Name of variation
+    """
+    doc_ref = db.collection("projects").document(project_name)
+    doc_ref.update({"variation_name": firestore.ArrayUnion([variation_name])})
+
+
+# saving project with NEW PROJECT NAME
+@callback(
+    Output("save_new_project", "children"),
     Input("save_to_firebase_btn", "n_clicks"),
     Input("project_name_input", "value"),
     Input("variation_name_input", "value"),
@@ -174,23 +239,44 @@ def send_project_data(project_name, variation_name):
 def save_to_firebase(n_clicks, project_name, variation_name, data):
     # send_data(data)
     if "save_to_firebase_btn" == ctx.triggered[0]["prop_id"].split(".")[0]:
+        new_project_(project_name, variation_name)
         send_data(data, project_name, variation_name)
-        # db.collection("potato").document("var").delete()
-        # print("sending data to firebase")
-        # doc_ref = db.collection("projects").document(project_name)
-        # doc_ref.set(
-        #     {
-        #         "project_name": project_name,
-        #         "variation_name": [variation_name],
-        #         "date": [datetime.datetime.now(tz=datetime.timezone.utc)],
-        #     }
-        # )
         return (
             dmc.Notification(
                 title="Save Successful",
                 id="save_notification",
                 action="show",
                 message="Your design variation has been saved to the database.",
+                icon=[DashIconify(icon="ic:round-celebration")],
+                color="green",
+            ),
+        )
+
+    else:
+        raise PreventUpdate
+
+
+# appending new variation to existing project
+@callback(
+    Output("save_new_variation", "children"),
+    Input("save_to_firebase_btn", "n_clicks"),
+    Input("project_select", "value"),
+    Input("variation_name_input", "value"),
+    State("analysis_store", "data"),
+    prevent_initial_call=True,
+)
+def save_to_firebase(n_clicks, project_name, variation_name, data):
+    if "save_to_firebase_btn" == ctx.triggered[0]["prop_id"].split(".")[0]:
+        append_to_project(project_name, variation_name)
+        send_data(data, project_name, variation_name)
+        return (
+            dmc.Notification(
+                title="Save Successful",
+                id="save_notification",
+                action="show",
+                message="Your design variation called {} has been saved to the {}.".format(
+                    variation_name, project_name
+                ),
                 icon=[DashIconify(icon="ic:round-celebration")],
                 color="green",
             ),
@@ -311,3 +397,43 @@ def update_project_name_input(proc_data):
 #         #         }
 #         #     }
 #         # )
+
+"""
+# TODO: finish this function
+- add data to projects collection.
+- check if project name is unique.
+- if not append variation name to project name.
+def send_project_data(project_name, variation_name):
+
+    db.collection("projects").add(
+        {
+            "project_name": project_name,
+            "variation_name": [variation_name],
+            "date": [datetime.datetime.now(tz=datetime.timezone.utc)],
+        }
+    )
+"""
+
+
+# storage.bucket("embodied-carbon.appspot.com").blob(
+#     "{}+{}.json".format(collection, document)
+# ).upload_from_filename(filename="data2.json", content_type="application/json")
+
+# df = pd.read_json(data, orient="split")
+
+# to_send = df_.to_json(orient="columns")
+# # Aggregated data
+# db.collection("aggregated").document(document).set()
+# # Main Data Collection
+# db.collection(collection).document(document).set(json.loads(to_send))
+
+
+# code for retreiving data from firestore
+"""
+    print(
+        bucket.blob("{}+{}.json".format(collection, document)).generate_signed_url(
+            datetime.timedelta(seconds=300), method="GET"
+        )
+    )
+
+"""
